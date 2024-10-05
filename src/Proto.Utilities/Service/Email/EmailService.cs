6717cddae2eb00643a8b230fec1b8a9e7c02fa9a
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-10-04 21:25:57 -0300

    Add default SMTP

--- src/Proto.Utilities/Service/Email/EmailService.cs
+++ src/Proto.Utilities/Service/Email/EmailService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Mail;
 using Proto.Arguments.General;
+using Proto.Arguments.General.Session;
 using Proto.Utilities.Interface.Service;
 
 namespace Proto.Utilities.Service;
@@ -9,8 +10,15 @@ public class EmailService : IEmailService
 {
     public async Task SendEmailAsync(string toEmail, string subject, string body, bool isBodyHtml, SmtpConfiguration? configuration)
     {
-        if (configuration == null)
-            throw new Exception("Configuração SMTP inválida");
+        configuration ??=
+            new SmtpConfiguration(SessionData.Configuration!["SMTP:Server"]!,
+            Convert.ToInt32(SessionData.Configuration!["SMTP:Port"]!),
+            SessionData.Configuration!["SMTP:DisplayName"]!,
+            SessionData.Configuration!["SMTP:From"]!,
+            SessionData.Configuration!["SMTP:Username"]!,
+            SessionData.Configuration!["SMTP:Password"]!,
+            Convert.ToBoolean(SessionData.Configuration!["SMTP:Ssl"]!),
+            SessionData.Configuration!["SMTP:EmailCopy"]!);
 
         using var mailMessage = new MailMessage();
         mailMessage.From = new MailAddress(configuration.FromEmail, configuration.DisplayName);
